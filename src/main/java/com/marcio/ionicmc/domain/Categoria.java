038d7cd76407aac0fce62f80b13f41878d7ce385
MarcioGomes78 <mjgomes1978@gmail.com>
2026-08-25 11:34:46 -0300

    H2 database and automatic database creation

--- src/main/java/com/marcio/ionicmc/domain/Categoria.java
+++ src/main/java/com/marcio/ionicmc/domain/Categoria.java
@@ -3,38 +3,50 @@ package com.marcio.ionicmc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Categoria implements Serializable{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
-	
+
 	public Categoria() {
 	}
-	
+
 	public Categoria(Integer id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -46,6 +58,5 @@ public class Categoria implements Serializable{
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
