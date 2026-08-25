c2b242b5d1eb33b2d88bcba0eec6c43d2f937930
MarcioGomes78 <mjgomes1978@gmail.com>
2026-08-25 00:13:17 -0300

    Create Project

---
+++ src/main/java/com/marcio/ionicmc/IonicmcApplication.java
@@ -0,0 +1,13 @@
+package com.marcio.ionicmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IonicmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IonicmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcio/ionicmc/IonicmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcio.ionicmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IonicmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
