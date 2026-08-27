fa2c7ae0e6479a278b96a93ef424298f093712ce
MarcioGomes78 <mjgomes1978@gmail.com>
2026-08-27 10:29:48 -0300

    State and citys

--- src/main/java/com/marcio/ionicmc/IonicmcApplication.java
+++ src/main/java/com/marcio/ionicmc/IonicmcApplication.java
@@ -8,41 +8,65 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.marcio.ionicmc.domain.Categoria;
+import com.marcio.ionicmc.domain.Cidade;
+import com.marcio.ionicmc.domain.Estado;
 import com.marcio.ionicmc.domain.Produto;
 import com.marcio.ionicmc.repositories.CategoriaRepository;
+import com.marcio.ionicmc.repositories.CidadeRepository;
+import com.marcio.ionicmc.repositories.EstadoRepository;
 import com.marcio.ionicmc.repositories.ProdutoRepository;
+
 
 @SpringBootApplication
 public class IonicmcApplication implements CommandLineRunner {
 
-	//importar o repositório de categoria
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private ProdutoRepository produtoRepository;
+    //importar o repositório de categoria
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(IonicmcApplication.class, args);
-	}
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(IonicmcApplication.class, args);
+    }
 
     @Override
     public void run(String... args) throws Exception {
 
-		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2 = new Categoria(null, "Escritório");
+        Categoria cat1 = new Categoria(null, "Informática");
+        Categoria cat2 = new Categoria(null, "Escritório");
 
-		Produto p1 = new Produto(null, "Computador", 2000.00);
-		Produto p2 = new Produto(null, "Mouse", 80.00);
-		Produto p3 = new Produto(null, "Teclado", 100.00);
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Mouse", 80.00);
+        Produto p3 = new Produto(null, "Teclado", 100.00);
 
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
 
-		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null, "Belo Horizonte", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
     }
 }
