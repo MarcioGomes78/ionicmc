fbe918cd9f5ec10b380f10058a4ef4871ce6632f
MarcioGomes78 <mjgomes1978@gmail.com>
2026-09-08 19:33:22 -0300

    Custom Validation: Unique email check during customer insertion.

--- src/main/java/com/marcio/ionicmc/repositories/ClienteRepository.java
+++ src/main/java/com/marcio/ionicmc/repositories/ClienteRepository.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Repository;
 
 import com.marcio.ionicmc.domain.Cliente;
 
+import org.springframework.transaction.annotation.Transactional;
+
 // aqui passamos o tipo da entidade e o tipo do id, Integer
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
-    // A anotação @Repository já é inferida pelo Spring
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 }
--- src/main/java/com/marcio/ionicmc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/marcio/ionicmc/services/validation/ClienteInsertValidator.java
@@ -3,8 +3,12 @@ package com.marcio.ionicmc.services.validation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.marcio.ionicmc.domain.Cliente;
 import com.marcio.ionicmc.domain.enums.TipoCliente;
 import com.marcio.ionicmc.dto.ClienteNewDTO;
+import com.marcio.ionicmc.repositories.ClienteRepository;
 import com.marcio.ionicmc.resources.exception.FieldMessage;
 import com.marcio.ionicmc.services.validation.utils.BR;
 
@@ -12,6 +16,9 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @Override
     public void initialize(ClienteInsert ann) {
@@ -28,6 +35,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
         }
         
+        Cliente cli = clienteRepository.findByEmail(objDto.getEmail());
+        if (cli != null){
+            list.add(new FieldMessage("email", "Email já cadastrado"));
+        }
         // incluir validação de email
         for (FieldMessage e: list){
             context.disableDefaultConstraintViolation();
